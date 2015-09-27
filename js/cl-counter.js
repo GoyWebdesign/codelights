c660dd8957ef516f20f02f7dd99cbeb1f7d63f11
Ruslan Sukhar <ruslan@sukhar.info>
2015-09-27 13:42:55 +0300

    cl-counter: getTextStates initial text animation states

--- js/cl-counter.js
+++ js/cl-counter.js
@@ -12,7 +12,7 @@
 		this.initials = [];
 		this.finals = [];
 		this.numberFormats = {};
-		this.textEditPaths = {};
+		this.textStates = {};
 		this.$container.find('.cl-counter-text-part').each(function(index, part){
 			this.$parts[index] = $(part);
 			this.now[index] = this.initials[index] = this.$parts[index].html() + '';
@@ -22,11 +22,12 @@
 			if (this.types[index] == 'number') {
 				this.numberFormats[index] = this.getFormat(this.initials[index], this.finals[index]);
 			} else if (this.types[index] == 'text') {
-				this.textEditPaths[index] = this.getTextEditPath(this.initials[index], this.finals[index]);
+				this.textStates[index] = this.getTextStates(this.initials[index], this.finals[index]);
 			}
 		}.bind(this));
 	};
 	CLCounter.prototype = {
+
 		/**
 		 * Get number format based on initial and final number strings
 		 * @param initial string
@@ -69,13 +70,13 @@
 			return format;
 		},
 		/**
-		 * Slightly modified Wagner-Fischer algorithm to obtain the shortest edit distance with certain actions
+		 * Slightly modified Wagner-Fischer algorithm to obtain the shortest edit distance with intermediate states
 		 * @param initial string The initial string
 		 * @param final string The final string
 		 * @returns {Array}
 		 * @private
 		 */
-		getTextEditPath: function(initial, final){
+		getTextStates: function(initial, final){
 			var dist = [],
 				i, j;
 			for (i = 0; i <= initial.length; i++) dist[i] = [i];
@@ -85,34 +86,28 @@
 					dist[i][j] = (initial[i - 1] === final[j - 1]) ? dist[i - 1][j - 1] : (Math.min(dist[i - 1][j], dist[i][j - 1], dist[i - 1][j - 1]) + 1);
 				}
 			}
-			// Obtaining the list of the optimal actions
-			var _actions = [];
-			for (i = initial.length, j = final.length; i > 0 || j > 0;) {
-				var min = Math.min.apply(Math, [dist[i - 1][j - 1], dist[i - 1][j], dist[i][j - 1]].filter(function(n){ return n !== undefined; })),
-					type = '';
-				if (min < dist[i][j]) {
-					type = 'M'; // Modify by default
-					if (min == dist[i][j - 1]) {
-						type = 'I'; // Insert
-					} else if (min == dist[i - 1][j]) {
-						type = 'R'; // Remove
-					}
-					_actions.unshift([type, i, j]);
+			// Obtaining the intermediate states
+			var states = [final],
+				posDiff = 0;
+			for (i = initial.length, j = final.length; i > 0 || j > 0; i--, j--) {
+				var min = dist[i][j];
+				if (i > 0) min = Math.min(min, dist[i - 1][j], dist[i - 1][j - 1] || min);
+				if (j > 0) min = Math.min(min, dist[i][j - 1]);
+				if (min >= dist[i][j]) continue;
+				if (min == dist[i][j - 1]) {
+					// Remove
+					states.unshift(states[0].substring(0, j - 1) + states[0].substring(j));
+					i++;
+				} else if (min == dist[i - 1][j - 1]) {
+					// Modify
+					states.unshift(states[0].substring(0, j - 1) + initial[i - 1] + states[0].substring(j));
+				} else if (min == dist[i - 1][j]) {
+					// Insert
+					states.unshift(states[0].substring(0, j) + initial[i - 1] + states[0].substring(j));
+					j++;
 				}
-				if (type != 'I') i--;
-				if (type != 'R') j--;
 			}
-			// Preparing a more detailed step-by-step guide
-			var posDiff = 0,
-				actions = [];
-			for (i = 0; i < _actions.length; i++) {
-				var pos = _actions[i][1] + posDiff + (_actions[i][0] != 'I' ? -1 : 0);
-				actions[i] = [_actions[i][0], pos];
-				if (_actions[i][0] != 'R') actions[i][2] = final.charAt(_actions[i][2] - 1);
-				if (_actions[i][0] == 'R') posDiff--;
-				if (_actions[i][0] == 'I') posDiff++;
-			}
-			return actions;
+			return states;
 		}
 	};
 	$.fn.clCounter = function(){
