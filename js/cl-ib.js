927a0972b0922498e59dac5bbc7e855c5d9233ca
Ruslan Sukhar <ruslan@sukhar.info>
2015-10-15 17:59:20 +0300

    cl-ib: removed scalables

--- js/cl-ib.js
+++ js/cl-ib.js
@@ -6,11 +6,6 @@
 		// Common dom elements
 		this.$container = $(container);
 
-		this.makeScalable({
-			'.cl-ib-content': ['border-width', 'padding'],
-			'.cl-ib-title': ['font-size'],
-			'.cl-ib-text': ['font-size']
-		});
 	};
 	CLIb.prototype = $cl.mutators.Scalable;
 	$cl.elements['cl-ib'] = CLIb;
