b075810d5d0c1fb4ab8842907ff3aaaa433abb30
Ruslan Sukhar <ruslan@sukhar.info>
2015-10-16 12:30:21 +0300

    cl-ib: size options applied

--- templates/elements/cl-ib.php
+++ templates/elements/cl-ib.php
@@ -15,32 +15,67 @@
  * @var $width string In pixels or percents: '100' / '100%'
  * @var $align string Text align: 'left' / 'center' / 'right'
  * @var $padding string In pixels or percents: '20px' / '10%'
- * @var $title_size string Title size: 'tiny' / 'small' / 'medium' / 'large' / 'huge'
- * @var $title_tag string Title tag name: 'h2' / 'h3' / 'h4' / 'h5' / 'div'
  * @var $easing string Easing CSS class name
  * @var $el_class string Extra class name
+ * @var $title_size string Title size in pixels
+ * @var $desc_size string Description size in pixels
+ * @var $title_mobile_size string Title size when window size is less than 600px
+ * @var $desc_mobile_size string Description size when window size is less than 600px
+ * @var $title_tag string Title tag name: 'h2' / 'h3' / 'h4' / 'h5' / 'div'
  */
 
-// Main element classes, inner css and additional attributes
+// Main element classes
 $classes = ' animation_' . $animation . ' ratio_' . $ratio . ' align_' . $align;
 
 // Altering whole element's div with anchor when it has a link
 $tag = empty( $link ) ? 'div' : 'a';
 $atts = empty( $link ) ? '' : cl_parse_vc_link( $link, TRUE );
 
+// Mobile css rules
+$mobile_css_rules = array();
+
 $title_html = '';
 if ( ! empty( $title ) ) {
 	if ( empty( $title_tag ) ) {
 		$title_tag = 'div';
 	}
-	$title_html .= '<' . $title_tag . ' class="cl-ib-title size_' . $title_size . '">' . $title . '</' . $title_tag . '>';
+	$title_html .= '<' . $title_tag . ' class="cl-ib-title"';
+	$title_html .= cl_prepare_inline_css( array(
+		'font-size' => $title_size,
+	) );
+	$title_html .= '>' . $title . '</' . $title_tag . '>';
 	$classes .= ' with_title';
+	if ( ! empty( $title_size ) AND ! empty( $title_mobile_size ) AND (int) $title_size != (int) $title_mobile_size ) {
+		$mobile_css_rules['.cl-ib-title'] = 'font-size:' . intval( $title_mobile_size ) . 'px !important;';
+	}
 }
 
-$text_html = '';
+$desc_html = '';
 if ( ! empty( $desc ) ) {
-	$text_html .= '<div class="cl-ib-text">' . $desc . '</div>';
+	$desc_html .= '<div class="cl-ib-desc"';
+	$desc_html .= cl_prepare_inline_css( array(
+		'font-size' => $desc_size,
+	) );
+	$desc_html .= '>' . $desc . '</div>';
 	$classes .= ' with_desc';
+	if ( ! empty( $desc_size ) AND ! empty( $desc_mobile_size ) AND (int) $desc_size != (int) $desc_mobile_size ) {
+		$mobile_css_rules['.cl-ib-desc'] = 'font-size:' . intval( $desc_mobile_size ) . 'px !important;';
+	}
+}
+
+// TODO Append properly styles from all the elemets to a single header-inserted css
+if ( ! empty( $mobile_css_rules ) ) {
+	// Unique styled element number
+	global $cl_custom_css_id;
+	$cl_custom_css_id = isset( $cl_custom_css_id ) ? ( $cl_custom_css_id + 1 ) : 1;
+	$classes .= ' cl_custom_css_' . $cl_custom_css_id;
+	$custom_css .= '<style type="text/css">';
+	$custom_css .= '@media only screen and (max-width: 599px) {';
+	foreach ( $mobile_css_rules as $mobile_css_elm => $mobile_css_rule ) {
+		$custom_css .= '.cl_custom_css_' . $cl_custom_css_id . ' ' . $mobile_css_elm . ' { ' . $mobile_css_rule . ' }';
+	}
+	$custom_css .= '}';
+	$custom_css .= '</style>';
 }
 
 if ( ! empty( $el_class ) ) {
@@ -67,8 +102,10 @@ $output .= '<div class="cl-ib-content"';
 $output .= cl_prepare_inline_css( array(
 	'padding' => $padding,
 ) );
-$output .= '><div class="cl-ib-content-h">' . $title_html . $text_html . '</div></div>';
+$output .= '><div class="cl-ib-content-h">' . $title_html . $desc_html . '</div></div>';
 $output .= '</div>';
 $output .= '</' . $tag . '>';
+
+$output .= $custom_css;
 
 echo $output;
