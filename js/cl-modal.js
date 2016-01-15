6a40c90c9f64d7f1f1caba0cafb9aa8a90a1ec78
Ruslan Sukhar <ruslan@sukhar.info>
2016-01-15 16:31:30 +0300

    cl-modal: initial animations

--- js/cl-modal.js
+++ js/cl-modal.js
@@ -14,13 +14,36 @@
 		this.$box = this.$container.find('.cl-modal-box');
 		this.$overlay = this.$container.find('.cl-modal-overlay');
 		this.$overlay.on('click', this.hide.bind(this));
+
+		this.timer = null;
+		this._events = {
+			afterShow: this.afterShow.bind(this),
+			afterHide: this.afterHide.bind(this)
+		};
 	};
 	CLModal.prototype = {
 		show: function(){
-			this.$overlay.appendTo(this.$body).show();
-			this.$box.appendTo(this.$body).show();
+			clearTimeout(this.timer);
+			this.$overlay.appendTo(this.$body).show().removeClass('active');
+			this.$box.appendTo(this.$body).show().removeClass('active');
+			this.timer = setTimeout(this._events.afterShow, 25);
+		},
+		afterShow: function(){
+			clearTimeout(this.timer);
+			this.$overlay.addClass('active');
+			this.$box.addClass('active');
 		},
 		hide: function(){
+			clearTimeout(this.timer);
+			var transitionEndEvent = (!!window.webkitURL) ? 'webkitTransitionEnd' : 'transitionend';
+			this.$box.one(transitionEndEvent, this._events.afterHide);
+			this.$overlay.removeClass('active');
+			this.$box.removeClass('active');
+			// Closing it anyway
+			this.timer = setTimeout(this._events.afterHide, 1000);
+		},
+		afterHide: function(){
+			clearTimeout(this.timer);
 			this.$overlay.appendTo(this.$container).hide();
 			this.$box.appendTo(this.$container).hide();
 		}
